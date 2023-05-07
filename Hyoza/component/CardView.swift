ea50fbc144b5dd499e0d21621d1e14c6bbf8f0de
soc06212 <soc06212@gmail.com>
2023-05-07 23:43:54 +0900

    Feat: CardView 컴포넌트 추가

---
+++ Hyoza/component/CardView.swift
@@ -0,0 +1,57 @@
+//
+//  CardView.swift
+//  Hyoza
+//
+//  Created by 최명근 on 2023/05/07.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+struct CardView<Content>: View where Content: View {
+    var backgroundColor: Color = Color("CardPrimaryColor")
+    var cornerRadius: CGFloat = 8.0
+    var shadowColor: Color = Color.black.opacity(0.3)
+    var shadowRadius: CGFloat = 4
+    var corners: UIRectCorner = .allCorners
+    
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        content()
+            .padding(EdgeInsets(top: 12, leading: 8, bottom: 12, trailing: 8))
+            .background(
+                Rectangle()
+                    .fill(backgroundColor)
+                    .cornerRadius(cornerRadius, corners: corners)
+                    .shadow(color: shadowColor, radius: shadowRadius, y: shadowRadius == 0 ? 0 : shadowRadius / 2)
+            )
+    }
+    
+}
+
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView {
+            Text("Hello, world!")
+        }
+        .frame(width: 200, height: 400)
+    }
+}
