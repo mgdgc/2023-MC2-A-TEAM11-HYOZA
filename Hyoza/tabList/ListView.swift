218caa463ac76307e21bda5557647d673991cc35
cutthebutter (Shannon) <uhoo1216@gmail.com>
2023-05-09 01:21:39 +0900

    Feat :"질문리스트 뷰 구현" / 코어데이터에서 데이터 불러와서 뷰에 fetch 구현 / Resolve : #9, #5

--- Hyoza/tabList/ListView.swift
+++ Hyoza/tabList/ListView.swift
@@ -15,32 +15,53 @@ struct ListView: View {
         animation: .default)
     
     private var items : FetchedResults<Question>
-
+    
     
     var body: some View {
         NavigationView {
             ScrollView{
                 LazyVStack {
                     header
-//                    ForEach(items) { item in
-                    ForEach(0..<100) { item in
+                    //코어데이터 생성을 위해 임의 배치
+                    Button(action : addItem){
+                        Label("Add Item", systemImage: "plus")
+                    }
+                    ForEach(items) { item in
+//                    ForEach(0..<100) { item in
                         RoundedRectangle(cornerRadius : 15)
                         //TODO : 프레임 스크린 넓이에서 가져오기
                             .frame(width: 350, height: 160)
                             .foregroundColor(.white)
                             .shadow( color : .gray, radius: 5, y :5)
                             .opacity(0.3)
-                            .overlay{CellContents}
+                            .overlay(CellContents(item : item))
                             .padding(.bottom, 20)
                         
                     }
-                  
                 }
-               
             }
         }
         
-        
+    }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Question(context: managedObjectContext)
+            newItem.id = Int64(Int(UUID().hashValue))
+            newItem.answer = "일기가 추가될 때, 일기를 특정할 수 있게 일기의 고유한 값을 저장하게 해야함, 즐겨찾기, 수정, 삭제 할때, 이 일기의 고유의 값을 전달할 수 있게 해야함, 전달받은 쪽에서 일기의 고유한 값이 있는지 확인하고 고유한 값에 해당되는 인덱스로 수정, 삭제, 즐겨찾기, 상태 업데이트를 구현 해줘야 함 위해"
+            newItem.answerTime = Date()
+            newItem.comment = "코멘트코멘트"
+            newItem.difficulty = 2
+            newItem.question = "일기가 추가될 때, 일기를 특정할 수 있게 일기의 고유한 값을 저장하게 해야함,"
+            newItem.timestamp = Date()
+            
+            do {
+                try managedObjectContext.save()
+            }catch{
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
     
     var header : some View {
@@ -54,43 +75,53 @@ struct ListView: View {
         .padding(.top, 43)
     }
     
-    var CellContents : some View{
+    // var CellContents : some View {
+    private struct CellContents : View {
+        
+        @State private var appear = false
+        var item : Question
         // TODO : 더 나은 방식으로 개선할 수 없나?
-        VStack(alignment : .leading) {
-            Text("2023년 5월 5일")
-                .font(.caption)
-                .frame(width: 320, alignment: .leading)
-                .lineLimit(1)
-                .padding(.top, 15)
-            
-            Text("최근에 가장 재미있게 본 유튜브 영상은 무엇인가요?")
-                .fontWeight(.bold)
-                .font(.headline)
-                .frame(width: 320, alignment: .leading)
-                .lineLimit(2)
-                .padding(.top, 5)
-            
-            Text("최근에 귀여운 강아지가 나오는 000 채널의 영상을 봤어요. 요새 동네들 산책하다보면 아주 귀여운 강아지가 ")
-                .fontWeight(.regular)
-                .font(.subheadline)
-                .frame(width: 320, alignment: .leading)
-                .lineLimit(2)
-                .padding(.top, 5)
-            Spacer()
-
+        
+        var body : some View{
+            VStack(alignment : .leading) {
+                Text(item.timestamp!, formatter : itemFormatter)
+                    .font(.caption)
+                    .frame(width: 320, alignment: .leading)
+                    .lineLimit(1)
+                    .padding(.top, 15)
+                
+                Text(item.question!)
+                    .fontWeight(.bold)
+                    .font(.headline)
+                    .frame(width: 320, alignment: .leading)
+                    .lineLimit(2)
+                    .padding(.top, 5)
+                
+                Text(item.answer!)
+                    .fontWeight(.regular)
+                    .font(.subheadline)
+                    .frame(width: 320, alignment: .leading)
+                    .lineLimit(2)
+                    .padding(.top, 5)
+                Spacer()
+                
+            }
         }
     }
+    
 }
+  
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
     
     
-
-
-
-
-
-
-struct ListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ListView()
+    struct ListView_Previews: PreviewProvider {
+        static var previews: some View {
+            ListView()
+        }
     }
-}
+
