b15331330620bb9e6b2012935c2b8aa44b5709b0
Benny(Gibaek Han) <hkb31415@gmail.com>
2023-05-09 05:41:43 +0900

    [Feat] TodayView
    23.05.09
    - 오늘의 질문 꾸러미
    - 오늘의 질문 선택(닫혀있는 3개의 질문 카드)
    
    오늘의 질문 꾸러미 아이콘을 누르면 3개의 질문 카드가 나타나도록 했습니다.

---
+++ Hyoza/tabToday/QuestionBoxView.swift
@@ -0,0 +1,42 @@
+//
+//  QuestionBoxView.swift
+//  Hyoza
+//
+//  Created by HAN GIBAEK on 2023/05/09.
+//
+
+import SwiftUI
+
+struct QuestionBoxView: View {
+    @Binding var zIndexQuestionBox: Double
+    @Binding var zIndexClosedQuestions: Double
+    
+    var body: some View {
+        ZStack{
+            Color.backGroundWhite
+            VStack {
+                Text("오늘의 질문 꾸러미")
+                    .font(.title)
+                Button(action: {
+                    withAnimation(.easeInOut(duration: 2)){
+                        zIndexQuestionBox = 0
+                        zIndexClosedQuestions = 1
+                    }
+                })
+                {
+                    Image(systemName: "shippingbox.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.backGroundLightOrange)
+                        .padding(20)
+                }
+            }
+        }
+    }
+}
+
+struct QuestionBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionBoxView(zIndexQuestionBox: .constant(1), zIndexClosedQuestions: .constant(0))
+    }
+}
