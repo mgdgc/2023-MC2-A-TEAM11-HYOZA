c00c138495f6beb3a040cbabe24bf0f9129be0fc
cutthebutter (Shannon) <uhoo1216@gmail.com>
2023-05-09 02:02:27 +0900

    주석 등 수정

--- Hyoza/tabList/ListView.swift
+++ Hyoza/tabList/ListView.swift
@@ -18,32 +18,31 @@ struct ListView: View {
     
     
     var body: some View {
-        NavigationView {
-            ScrollView{
-                LazyVStack {
+        ScrollView{
+            LazyVStack {
                     header
-                    //코어데이터 생성을 위해 임의 배치
-                    Button(action : addItem){
-                        Label("Add Item", systemImage: "plus")
-                    }
-                    ForEach(items) { item in
-//                    ForEach(0..<100) { item in
-                        RoundedRectangle(cornerRadius : 15)
-                        //TODO : 프레임 스크린 넓이에서 가져오기
-                            .frame(width: 350, height: 160)
-                            .foregroundColor(.white)
-                            .shadow( color : .gray, radius: 5, y :5)
-                            .opacity(0.3)
-                            .overlay(CellContents(item : item))
-                            .padding(.bottom, 20)
-                        
-                    }
+                //MARK: 삭제예정 : 코어데이터 생성을 위해 임의 배치
+//                Button(action : addItem){
+//                    Label("Add Item", systemImage: "plus")
+//                }
+                //MARK: 삭제예정 :코어데이터 생성을 위해 임의 배치
+                ForEach(items) { item in
+                    RoundedRectangle(cornerRadius : 15)
+                    //TODO: 프레임 스크린 넓이에서 가져오기
+                        .frame(width: 350, height: 160)
+                        .foregroundColor(.white)
+                        .shadow( color : .gray, radius: 5, y :5)
+                        .opacity(0.3)
+                        .overlay(CellContents(item : item))
+                        .padding(.bottom, 20)
+                    
                 }
             }
         }
-        
     }
+
     
+    // MARK: 삭제예정 :데이터가 들어오면 삭제될 부분
     private func addItem() {
         withAnimation {
             let newItem = Question(context: managedObjectContext)
@@ -63,50 +62,50 @@ struct ListView: View {
             }
         }
     }
+    //MARK: 삭제예정 :데이터가 들어오면 삭제될 부분
+}
+
+private var header : some View {
+    HStack {
+        Text("질문 리스트")
+            .font(.largeTitle)
+            .bold()
+        Spacer()
+    }
+    .padding(.leading, 20)
+    .padding(.top, 43)
+}
+
+private struct CellContents : View {
     
-    var header : some View {
-        HStack {
-            Text("질문 리스트")
-                .font(.largeTitle)
-                .bold()
+    @State private var appear = false
+    var item : Question
+    // TODO: 더 나은 방식으로 개선할 수 없나?
+    var body : some View{
+        VStack(alignment : .leading) {
+            Text(item.timestamp!, formatter : itemFormatter)
+                .font(.caption)
+                .frame(width: 320, alignment: .leading)
+                .lineLimit(1)
+                .padding(.top, 15)
+            
+            Text(item.question!)
+                .fontWeight(.bold)
+                .font(.headline)
+                .frame(width: 320, alignment: .leading)
+                .lineLimit(2)
+                .padding(.top, 5)
+            
+            Text(item.answer!)
+                .fontWeight(.regular)
+                .font(.subheadline)
+                .frame(width: 320, alignment: .leading)
+                .lineLimit(2)
+                .padding(.top, 5)
             Spacer()
-        }
-        .padding(.leading, 20)
-        .padding(.top, 43)
-    }
-    
-    private struct CellContents : View {
-        
-        @State private var appear = false
-        var item : Question
-        // TODO : 더 나은 방식으로 개선할 수 없나?
-        var body : some View{
-            VStack(alignment : .leading) {
-                Text(item.timestamp!, formatter : itemFormatter)
-                    .font(.caption)
-                    .frame(width: 320, alignment: .leading)
-                    .lineLimit(1)
-                    .padding(.top, 15)
-                
-                Text(item.question!)
-                    .fontWeight(.bold)
-                    .font(.headline)
-                    .frame(width: 320, alignment: .leading)
-                    .lineLimit(2)
-                    .padding(.top, 5)
-                
-                Text(item.answer!)
-                    .fontWeight(.regular)
-                    .font(.subheadline)
-                    .frame(width: 320, alignment: .leading)
-                    .lineLimit(2)
-                    .padding(.top, 5)
-                Spacer()
-                
-            }
+            
         }
     }
-    
 }
   
 private let itemFormatter: DateFormatter = {
@@ -117,9 +116,9 @@ private let itemFormatter: DateFormatter = {
 }()
     
     
-    struct ListView_Previews: PreviewProvider {
-        static var previews: some View {
-            ListView()
-        }
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
     }
+}
 
