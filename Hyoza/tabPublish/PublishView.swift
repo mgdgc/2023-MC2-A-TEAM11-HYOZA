1fab7a09148f65ecf10c0e804bcdc334fa54ddf6
sei <vanism7557@gmail.com>
2023-05-09 15:37:07 +0900

    feat: #6 - header - PublishView

--- Hyoza/tabPublish/PublishView.swift
+++ Hyoza/tabPublish/PublishView.swift
@@ -12,7 +12,27 @@ struct PublishView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.backgroundColor
+            VStack(alignment: .leading, spacing: .zero) {
+                header
+            }
+        }
+        .ignoresSafeArea(edges: .top)
+        
+    }
+    
+    var header: some View {
+        HStack {
+            Text("자서전 출판")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(Color.textColor)
+            Spacer()
+        }
+        .padding(.leading, 20)
+        .padding(.top, 90)
+    
     }
 }
 
