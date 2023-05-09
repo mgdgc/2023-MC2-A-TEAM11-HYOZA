a65ed12bb15e7dacf9581fd403d03d7266889674
sei <vanism7557@gmail.com>
2023-05-09 15:13:32 +0900

    fix: Color 관련 extension 수정

--- Hyoza/extensions/ColorExtension.swift
+++ Hyoza/extensions/ColorExtension.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 extension Color {
-    static var textColor: Color? { return Color(CustomColor.TextColor.rawValue) }
-    static var backgroundColor: Color? { return Color(CustomColor.BackgroundColor.rawValue) }
-    static var buttonColor: Color? { return Color(CustomColor.ButtonColor.rawValue) }
-    static var buttonTextColor: Color? { return Color(CustomColor.ButtonTextColor.rawValue) }
-    static var cardPrimaryColor: Color? { return Color(CustomColor.CardPrimaryColor.rawValue) }
-    static var cardSecondaryColor: Color? { return Color(CustomColor.CardSecondaryColor.rawValue) }
+    static var textColor: Color { return Color(CustomColor.TextColor.rawValue) }
+    static var backgroundColor: Color { return Color(CustomColor.BackgroundColor.rawValue) }
+    static var buttonColor: Color { return Color(CustomColor.ButtonColor.rawValue) }
+    static var buttonTextColor: Color { return Color(CustomColor.ButtonTextColor.rawValue) }
+    static var cardPrimaryColor: Color { return Color(CustomColor.CardPrimaryColor.rawValue) }
+    static var cardSecondaryColor: Color { return Color(CustomColor.CardSecondaryColor.rawValue) }
 }
