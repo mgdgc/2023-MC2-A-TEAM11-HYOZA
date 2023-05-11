4a7714ae8562d4c4af4c975a24ed3f38bed45c95
cutthebutter (Shannon) <uhoo1216@gmail.com>
2023-05-11 17:01:22 +0900

    feat: "질문리스트뷰"-리스트뷰에서 navigationLink 인자 수정

--- Hyoza/tabList/ListView.swift
+++ Hyoza/tabList/ListView.swift
@@ -46,7 +46,7 @@ struct ListView: View {
                         //MARK: 삭제예정 :코어데이터 생성을 위해 임의 배치
                         //아이템을 만들어주기 위한 RoundedRectangle
                         ForEach(items) { item in
-                            NavigationLink(destination : QnAView(isEditing: false, textValue: item.question!, comment: item.comment!)){
+                            NavigationLink(destination : QnAView(data:item)){
                                 RoundedRectangle(cornerRadius : 15)
                                 //viewWidth하는
                                     .frame(width: viewWidth, height: 160)
@@ -63,6 +63,7 @@ struct ListView: View {
                 }
             }
             .navigationTitle("질문 리스트")
+
             .navigationBarTitleDisplayMode(.large)
             .navigationBarTitleTextColor(.textColor)
         }
